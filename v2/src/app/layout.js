ceae04f86cef614979647553957c4694914bfe53
Aroblazeur <rekdi94@gmail.com>
2026-07-20 22:35:15 +0200

    fix: declare interface language as French

--- v2/src/app/layout.js
+++ v2/src/app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="fr" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body>
         <AuthProvider>{children}</AuthProvider>
       </body>
